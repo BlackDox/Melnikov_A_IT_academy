7cf4c8df818ac77a941dee448eacb48fa8d09dab
nout-hp <black_dox@mail.ru>
2017-11-25 15:46:17 +0700

    Initial commit 2

--- src/main/java/com/jcourse/melnikov/MyClass.java
+++ src/main/java/com/jcourse/melnikov/MyClass.java
@@ -5,11 +5,12 @@ import java.nio.charset.Charset;
 
 public class MyClass {
     public static void main(String[] args) throws IOException {
+        String test = new String("testing".getBytes(), Charset.forName("UTF-8"));
+        System.out.println(System.getProperties());
+
         /*BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         reader.readLine();
 
         reader.close();*/
-
-        System.out.println(System.getProperties());
     }
 }
