3f3f0e1a2526b60ccd9594c9f62c06f1f51ca6f5
nout-hp <black_dox@mail.ru>
2017-11-25 15:33:59 +0700

    Initial commit

---
+++ src/main/java/com/jcourse/melnikov/MyClass.java
@@ -0,0 +1,15 @@
+package com.jcourse.melnikov;
+
+import java.io.*;
+import java.nio.charset.Charset;
+
+public class MyClass {
+    public static void main(String[] args) throws IOException {
+        /*BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        reader.readLine();
+
+        reader.close();*/
+
+        System.out.println(System.getProperties());
+    }
+}
